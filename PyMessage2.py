4e6f714bd0f4f3a55ec5c739e6224c83e4b4feaa
sbatzel <stbatzel@gmail.com>
2015-09-08 00:17:28 -0400

    Try again...

---
+++ PyMessage2.py
@@ -0,0 +1,15 @@
+import socket
+
+def listenerThread():
+    
+
+global sock
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((socket.gethostname(), 1551))
+
+sock.listen(1)
+conn, addr = sock.accept()
+
+while 1:
+    mess = conn.recv(1024)
+    print(str(mess.decode))
